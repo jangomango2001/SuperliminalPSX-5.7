573cf32389f9440b6d7cd0f844ea78a7678503e4
jangomango2001 <58989530+jangomango2001@users.noreply.github.com>
2026-01-31 18:16:08 +0500

    Menu Changes, Pawn Fixes

--- Source/SuperliminalPSX/Public/VRCharacter.h
+++ Source/SuperliminalPSX/Public/VRCharacter.h
@@ -25,6 +25,7 @@ protected:
 private:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "Playersettings")
 	UCameraComponent* activeCam = NULL;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
