f1c3c768122b818137adbcad5c52d121c720f55a
jangomango2001 <58989530+jangomango2001@users.noreply.github.com>
2026-02-25 00:00:28 +0500

    Reset Puzzle, Reset Player, Object Block

--- Source/SuperliminalPSX/SuperliminalPSX.Build.cs
+++ Source/SuperliminalPSX/SuperliminalPSX.Build.cs
@@ -8,7 +8,7 @@ public class SuperliminalPSX : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "RenderCore", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
